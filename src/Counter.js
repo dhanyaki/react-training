056ff0fce63f86ee1624eb9dc95faeed4a7d56ad
dhanyaki <dhanyak46@gmail.com>
2024-05-25 06:45:52 +0530

    basic counter

--- src/Counter.js
+++ src/Counter.js
@@ -9,20 +9,43 @@ export default class Counter extends Component {
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
   }
-  increment(){
-    this.setState({counter: this.state.counter + 1});
+  increment() {
+    if (this.state.counter + 1 > 10) {
+      this.setState({ counter:1 });
+    }
+    else {
+
+      this.setState({ counter: this.state.counter + 1 });
+    }
+
+
   }
-  decrement(){
-    this.setState({counter: this.state.counter - 1});
+
+
+
+decrement()
+{
+  if (this.state.counter - 1 < 0) {
+    this.setState({ counter: 0});
   }
-  render() {
-    return (
-        <>
-        <button onClick={this.increment}>+1</button>
-        <button onClick={this.decrement}>-1</button>
-        <p>{this.state.counter}</p>
-        </>
-        
-    );
+  else {
+
+    this.setState({ counter: this.state.counter - 1 });
   }
 }
+
+
+
+render(){
+return (
+  <>
+    <button onClick={this.increment}>+1</button>
+    <button onClick={this.decrement}>-1</button>
+    <p>{this.state.counter}</p>
+  </>
+
+);
+}
+}
+
+
