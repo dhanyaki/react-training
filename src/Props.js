24b06b1cea4cd57824a027e58caa9573ddfdc954
TUMPILLI SAMBA MURTHY <sambatumpilli@gmail.com>
2024-05-25 07:42:23 +0530

    state lifting basic example

--- src/Props.js
+++ src/Props.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Props(props) {
-    const {name, age} = props;
+  const [name, setName] = useState("samba");
+  const [age] = useState(25);
   return (
-    <div>{name} {age} {props.gender}</div>
-  )
+    <>
+      <div>
+        {name} {age} {props.gender}
+      </div>
+      <PropChild {...{ name, setName }} />
+    </>
+  );
+}
+function PropChild({ name, setName }) {
+  return (
+    <input
+      name="name"
+      value={name}
+      onChange={(e) => {
+        setName(e.target.value);
+      }}
+    />
+  );
 }
 
-export default Props
+export default Props;
