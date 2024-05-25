cb6cc009d8bdfdaedb9de931479913b5bbf896f8
TUMPILLI SAMBA MURTHY <sambatumpilli@gmail.com>
2024-05-25 06:52:35 +0530

    counter

--- src/Counter.js
+++ src/Counter.js
@@ -9,20 +9,23 @@ export default class Counter extends Component {
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
   }
-  increment(){
-    this.setState({counter: this.state.counter + 1});
+  increment() {
+    if (this.state.counter +1 >= 11) {
+      this.setState({ counter: 1 });
+    } else {
+      this.setState({ counter: this.state.counter + 1 });
+    }
   }
-  decrement(){
-    this.setState({counter: this.state.counter - 1});
+  decrement() {
+    this.setState({ counter: this.state.counter - 1 });
   }
   render() {
     return (
-        <>
+      <>
         <button onClick={this.increment}>+1</button>
         <button onClick={this.decrement}>-1</button>
         <p>{this.state.counter}</p>
-        </>
-        
+      </>
     );
   }
 }
